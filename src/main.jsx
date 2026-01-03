585277e4748e36ff930b6e790910bd00fc0af5d9
cereterall-coder <cereterall@gmail.com>
2026-01-03 13:48:51 -0500

    Chore: Integrate ErrorBoundary in main entry point

--- src/main.jsx
+++ src/main.jsx
@@ -4,10 +4,14 @@ import { BrowserRouter } from 'react-router-dom'
 import App from './App.jsx'
 import './styles/global.css'
 
+import ErrorBoundary from './components/ErrorBoundary';
+
 createRoot(document.getElementById('root')).render(
     <StrictMode>
-        <BrowserRouter>
-            <App />
-        </BrowserRouter>
+        <ErrorBoundary>
+            <BrowserRouter>
+                <App />
+            </BrowserRouter>
+        </ErrorBoundary>
     </StrictMode>,
 )
